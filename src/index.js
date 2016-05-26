d03baa75788ec0409825e8226c8d9fa3a6d4eecb
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-05-26 17:26:53 +0200

    Sounder error handling.

--- src/index.js
+++ src/index.js
@@ -36,22 +36,21 @@ app.use(function(req, res, next) {
 
 // development error handler, will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        console.log(err);
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message
+        });
     });
-  });
 }
 
 // production error handler, no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message
+    });
 });
 
 
