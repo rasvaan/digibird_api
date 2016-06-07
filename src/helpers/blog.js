203b53af8958db4990a7a1237fbfd56adfc8e2fd
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-06-07 11:33:58 +0200

    Cleaning and moved credentials to a sepperate file.

--- src/helpers/blog.js
+++ src/helpers/blog.js
@@ -1,54 +1,31 @@
 /*******************************************************************************
-DigiBird Utilities
+DigiBird Blog
 
 Functions used by multiple javascript files. These are used throughout the
 application as helper tools.
 *******************************************************************************/
 
-// OPTION 1
-var writeOutputToFile = function (debugFile, dataInfo, data) {
-  var fs = require('fs');
-  var util = require('util');
-  var path = require('path');
+module.exports = {
+    dumpToFile: function (debugFile, dataInfo, data) {
+        var fs = require('fs');
+        var util = require('util');
+        var path = require('path');
 
-  var logFile = fs.createWriteStream(path.resolve(__dirname, '..', debugFile),
-                  {flags: 'w'});
+        var logFile = fs.createWriteStream(path.resolve(__dirname, '..', debugFile),
+                    {flags: 'w'});
 
-  var date = new Date();
-  var harvestDate = date.getFullYear() + "-" + date.getMonth() + "-" +
-                      date.getDate() + " " + date.getHours() + ":" +
-                      date.getMinutes() + ":" + date.getSeconds();
+        var date = new Date();
+        var harvestDate = date.getFullYear() + "-" + date.getMonth() + "-" +
+            date.getDate() + " " + date.getHours() + ":" +
+            date.getMinutes() + ":" + date.getSeconds();
 
-  logFile.write("-------------------------\n");
-  logFile.write(dataInfo + " " + harvestDate + "\n");
-  logFile.write("-------------------------\n");
-  logFile.write(util.format(data));
-  logFile.write("\n-------------------------\n");
+        logFile.write("-------------------------\n");
+        logFile.write(dataInfo + " " + harvestDate + "\n");
+        logFile.write("-------------------------\n");
+        logFile.write(util.format(data));
+        logFile.write("\n-------------------------\n");
+
+        // this closes to soon, why is the write async?
+        // logFile.close();
+    }
 };
-exports.dumpToFile = writeOutputToFile;
-
-// OPTION 2
-// module.exports = {
-//   writeOutputToFile: function (debugFile, dataInfo, data) {
-//     var fs = require('fs');
-//     var util = require('util');
-//     var path = require('path');
-//
-//     var logFile = fs.createWriteStream(path.resolve(__dirname, '..', debugFile),
-//                     {flags: 'w'});
-//
-//     var date = new Date();
-//     var harvestDate = date.getFullYear() + "-" + date.getMonth() + "-" +
-//                         date.getDate() + " " + date.getHours() + ":" +
-//                         date.getMinutes() + ":" + date.getSeconds();
-//
-//     logFile.write("-------------------------\n");
-//     logFile.write(dataInfo + " " + harvestDate + "\n");
-//     logFile.write("-------------------------\n");
-//     logFile.write(util.format(data));
-//     logFile.write("\n-------------------------\n");
-//   }//,
-//   // otherHelperFunction: function () {
-//   //   // whatever
-//   // }
-// };
