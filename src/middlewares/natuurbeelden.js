8b79aa3bbad72f795af1dd07c2d43b9ea79b28c8
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2016-08-03 13:23:19 +0200

    POST request to BenG using request-promise-native - rearranged code, still gives 404 error

--- src/middlewares/natuurbeelden.js
+++ src/middlewares/natuurbeelden.js
@@ -17,39 +17,41 @@ var natuurbeeldenUtils = require('../helpers/natuurbeelden');
 module.exports = {
     getBenGMetadata: function() {
 
-      // configuration of POST and sending the request
-      request({
-          url: 'http://in.beeldengeluid.nl/collectie/search', // URL to hit
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          qs: {
-            q: 'bird',
-            page: '1',
-            publiclyViewableResultsOnly: 'true',
-            digitalViewableResultsOnly: 'false'
-          }, // query string data
-          body: {
-            phrase:'bird',
-            page:'1',
-            numkeyframes:5,
-            sorting:'SORT-DEF',
-            mediaType:'ALL_MEDIA',
-            pagesize:12,
-            startdate:null,
-            enddate:null,
-            publiclyViewableResultsOnly:'true',
-            digitalViewableResultsOnly:'false',
-            termFilters:'{}'
-          }, // body of the request
-          json: true // json serialization on the body object
-      }, function(error, response, body){
-          if(error) {
-              console.log(error);
-          } else {
-              console.log(response.statusCode, body);
-          }
+      // configuration of POST request
+      var options = {
+        method: 'POST',
+        url: 'http://in.beeldengeluid.nl/collectie/search', // URL to hit
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        qs: {
+          q: 'bird',
+          page: '1',
+          publiclyViewableResultsOnly: 'true',
+          digitalViewableResultsOnly: 'false'
+        }, // query string data
+        body: {
+          phrase:'bird',
+          page:'1',
+          numkeyframes:5,
+          sorting:'SORT-DEF',
+          mediaType:'ALL_MEDIA',
+          pagesize:12,
+          startdate:null,
+          enddate:null,
+          publiclyViewableResultsOnly:'true',
+          digitalViewableResultsOnly:'false',
+          termFilters:'{}'
+        }, // body of the request
+        json: true // json serialization on the body = stringifies body to JSON
+      };
+
+      request(options)
+      .then(function (parsedBody) {
+          console.log(response.statusCode, parsedBody);
+      })
+      .catch(function (error) {
+          console.log(error);
       });
     }
 }
