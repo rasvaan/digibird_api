f82012b465f66fe8ae9f3363aadf5d159251a21b
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2016-08-03 15:36:12 +0200

    connection to search API BenG without parsing the JSON result

--- src/middlewares/natuurbeelden.js
+++ src/middlewares/natuurbeelden.js
@@ -16,42 +16,37 @@ var natuurbeeldenUtils = require('../helpers/natuurbeelden');
 
 module.exports = {
     getBenGMetadata: function() {
-
+      var URL = 'http://in.beeldengeluid.nl/collectie/api/search/';
+      var searchPhrase = 'bird';
+      var requestBody = {
+        phrase:searchPhrase,
+        page:'1',
+        numkeyframes:5,
+        sorting:'SORT-DEF',
+        mediaType:'ALL_MEDIA',
+        pagesize:12,
+        startdate:null,
+        enddate:null,
+        publiclyViewableResultsOnly:'true',
+        digitalViewableResultsOnly:'false',
+        termFilters:{}
+      };
       // configuration of POST request
       var options = {
         method: 'POST',
-        url: 'http://in.beeldengeluid.nl/collectie/search/', // URL to hit
+        uri: URL, // URL to hit
         headers: {
-          'Accept': 'application/json, text/plain, */*',
-          'Accept-Encoding': 'gzip, deflate',
-          'Content-Type': 'application/json;charset=utf-8'
-        },
-        qs: {
-          q: 'bird',
-          page: '1',
-          publiclyViewableResultsOnly: 'true',
-          digitalViewableResultsOnly: 'false'
-        }, // query string data
-        body: {
-          phrase:'bird',
-          page:'1',
-          numkeyframes:5,
-          sorting:'SORT-DEF',
-          mediaType:'ALL_MEDIA',
-          pagesize:12,
-          startdate:null,
-          enddate:null,
-          publiclyViewableResultsOnly:'true',
-          digitalViewableResultsOnly:'false',
-          termFilters:{}
-        }, // body of the request
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+        body: requestBody,//JSON.stringify(requestBody), // body of the request
         json: true // json serialization on the body = stringifies body to JSON
       };
 
       request(options)
-      .then(function (parsedBody) {
+      .then(function (response) {
         console.log("PARSED BODY!!! start /n ========================= /n");
-        console.log(response.statusCode, parsedBody);
+        console.log(response);
         console.log("PARSED BODY!!! end /n ========================= /n");
 
       })
