c136196b51002112292ce39b6a47a4a74f7ed337
Cristina-Iulia Bucur <bucurcristina@yahoo.com>
2016-08-03 11:31:49 +0200

    POST request to BenG search - connection with request package; response: 405: POST request not supported

--- src/middlewares/natuurbeelden.js
+++ src/middlewares/natuurbeelden.js
@@ -10,44 +10,55 @@ node ./src/middlewares/natuurbeelden.js
 *******************************************************************************/
 
 var http = require('http');
+var request = require('request');
 var winston = require('winston');
 var natuurbeeldenUtils = require('../helpers/natuurbeelden');
-var credentials = require('./natuurbeelden_credentials');
 
 module.exports = {
     getBenGMetadata: function() {
 
-        // the post string for the request
-        var postData = JSON.stringify({
-          "phrase":"bird","page":"1","numkeyframes":5,"sorting":"SORT-DEF",
-          "mediaType":"ALL_MEDIA","pagesize":12,"startdate":null,"enddate":null,
-          "publiclyViewableResultsOnly":"true","digitalViewableResultsOnly":"false",
-          "termFilters":"{}"
-        });
-        console.log('postData ok' );
-        // options to indicate where the post request is made
-        var postOptions = {
-          host: 'in.beeldengeluid.nl',
-          port: 80,
-          path: '/collectie/search/?q=&publiclyViewableResultsOnly=true',
+      //Lets configure and request
+      request({
+          url: 'http://in.beeldengeluid.nl/collectie/search/', //URL to hit
           method: 'POST',
           headers: {
-            'Content-Type': 'application/json;charset=utf-8',
-            'Content-Length': Buffer.byteLength(postData)
+            'Content-Type': 'application/json',
+          },
+          qs: {
+            q: '',
+            publiclyViewableResultsOnly: 'true'
+          }, //Query string data
+          //Lets post the following key/values as form
+          json: {
+            "phrase":"bird",
+            "page":"1",
+            "numkeyframes":5,
+            "sorting":"SORT-DEF",
+            "mediaType":"ALL_MEDIA",
+            "pagesize":12,
+            "startdate":null,
+            "enddate":null,
+            "publiclyViewableResultsOnly":"true",
+            "digitalViewableResultsOnly":"false",
+            "termFilters":"{}"
           }
-        }
-
-        // set up the POST request
-        var postRequest = http.request(postOptions, function(response) {
-           response.setEncoding('application/json');
-           response.on('postData', function (chunk) {
-               console.log('Response: ' + chunk);
-           });
-        });
-
-         // post the data
-         postRequest.write(postData);
-         postRequest.end();
-
+      }, function(error, response, body){
+          if(error) {
+              console.log(error);
+          } else {
+              console.log(response.statusCode, body);
+          }
+      });
     }
 }
+        // // options to indicate where the post request is made
+        // var postOptions = {
+        //   host: 'in.beeldengeluid.nl',
+        //   port: 80,
+        //   path: '/collectie/search/?q=&publiclyViewableResultsOnly=true',
+        //   method: 'POST',
+        //   headers: {
+        //     'Content-Type': 'application/json;charset=utf-8',
+        //     'Content-Length': Buffer.byteLength(postData)
+        //   }
+        // }
