332a0cbd5446313d3ea89fc62eb4079fb4d77043
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-08-08 14:39:27 +0200

    More specific css selectors.

--- client/js/blog.js
+++ client/js/blog.js
@@ -2,6 +2,6 @@ var $ = require('jquery');
 window.$ = window.jQuery = $; // in order to make jquery available in the global space for bootstrap
 
 require('bootstrap-loader');
-require("font-awesome-sass-loader");
-require("../css/digibird.css");
-require("../css/digibird-blog.css");
+require('font-awesome-sass-loader');
+require('../css/digibird.css');
+require('../css/digibird-blog.css');
