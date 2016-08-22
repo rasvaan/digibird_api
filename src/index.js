5e5f091c8925e509fb385e7990fd044171363eb0
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-08-22 10:19:48 +0200

    Created simple api returning blogposts.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-'use strict';
-
 var express = require('express');
-var exphbs  = require('express-handlebars');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -12,19 +9,14 @@ var routes = require('./routes');
 
 var app = express();
 
-app.engine('.hbs', exphbs({extname: '.hbs'}));
-app.set('view engine', '.hbs');
 routes.set(app);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
-app.use(express.static(path.resolve(__dirname, '..', 'static')));
-app.use(express.static(path.resolve(__dirname, '..', 'assets')));
-
 // logging to file
-winston.add(winston.transports.File, {filename: 'digibird.log'});
+winston.add(winston.transports.File, {filename: 'digibird_api.log'});
 
 // scheduled tasks
 setInterval(function() {
@@ -40,27 +32,11 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
-// development error handler, will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        winston.log(err);
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message
-        });
-    });
-}
-
-// production error handler, no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message
-    });
+    winston.log(err);
 });
 
 
-app.listen(3000, function() {
-    winston.log('info', 'Started server on 3000.');
+app.listen(3030, function() {
+    winston.log('info', 'Started server on 3030.');
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,6 @@
 var blogUtils = require('../helpers/blog');
 
 module.exports.set = function(app) {
-
-    app.get('/', function(req, res) {
-        res.render('home');
-    });
-
-    app.get('/people', function(req, res) {
-        res.render('people');
-    });
-
     app.get('/blog', function(req, res) {
         //   get cached blog posts
         var blogPosts = blogUtils.readCacheJson();
@@ -18,9 +9,7 @@ module.exports.set = function(app) {
         for (var i=0; i<blogPosts.length; i++)
             blogPosts[i].content = blogPosts[i].content.replace(/\s*\[.*?\]\s*/g, '');
 
-        var data = { posts: blogPosts };
-
         // send the blog posts to the client 'blog' page
-        res.render('blog', data);
+        res.send(JSON.stringify({ posts: blogPosts }));
     });
 };
