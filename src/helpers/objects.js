573d015de9608a24d747bfe8185acf8e23c1c4a1
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2016-09-20 13:39:09 +0200

    Processing results obtained from Rijksmuseum LD, resolves #17.

--- src/helpers/objects.js
+++ src/helpers/objects.js
@@ -8,6 +8,9 @@ var soortenRegister = require('../middlewares/soorten-register');
 var xenoCanto = require('../middlewares/xeno-canto-api');
 var tripleStore = require('../middlewares/triple-store');
 var Results = require('../classes/Results');
+var Aggregation = require('../classes/Aggregation');
+var CulturalObject = require('../classes/CulturalObject');
+var WebResource = require('../classes/WebResource');
 
 module.exports = {
   get: function(parameters) {
@@ -47,18 +50,12 @@ module.exports = {
     switch (platform.id) {
       case 'soortenregister': {
         return soortenRegister.request(parameters).then((aggregations) => {
-          let soortenResults = new Results();
-          soortenResults.addAggregations(aggregations);
-          soortenResults.addPlatform(platform);
-          return soortenResults;
+          return new Results(aggregations, [platform]);
         });
       }
       case 'xeno-canto': {
         return xenoCanto.request(parameters).then((aggregations) => {
-          let xenoResults = new Results();
-          xenoResults.addAggregations(aggregations);
-          xenoResults.addPlatform(platform);
-          return xenoResults;
+          return new Results(aggregations, [platform]);
         });
       }
       default: {
@@ -70,19 +67,18 @@ module.exports = {
     }
   },
   objectsTripleStore: function(platform, parameters) {
-    console.log('retrieving objects from', platform.id, ' with parameters ', parameters);
-    // const query = this.sparqlObjectQueries()[statistic];
+    let _this = this;
+
     switch(platform.id) {
       case 'rijksmuseum': {
+        // only filter, since we have no suitable concepts to query for
         const filter = this.mergeQueryParameters(parameters);
         const query = this.sparqlObjectQueries(filter)['filter_desciption'];
-        console.log('Query', query.query);
-        return tripleStore.query(platform, query.query).then(function(value) {
-          console.log('results', value);
-          // process the results
-          let rijksmuseumResults = new Results();
-          rijksmuseumResults.addPlatform(platform);
-          return rijksmuseumResults;
+
+        return tripleStore.query(platform, query.query).then((values) => {
+          return _this.processSparqlAggregations(values, 'dctype:Image');
+        }).then((aggregations) => {
+          return new Results(aggregations, [platform]);
         });
       }
       default: {
@@ -100,9 +96,24 @@ module.exports = {
       case 'species': return `${parameters.genus} ${parameters.species}`;
       case 'common': return parameters.common_name;
     }
+    // TODO: for the Rijksmuseum translate scientific names to common?
+  },
+  processSparqlAggregations: function(results, type) {
+    let aggregations = [];
+
+    for (let i=0; i<results.length; i++) {
+      const result = results[i];
+
+      aggregations[aggregations.length] = new Aggregation(
+        result.aggregation.value,
+        new CulturalObject(result.object.value),
+        new WebResource(result.view.value, type)
+      );
+    }
+
+    return aggregations;
   },
   sparqlObjectQueries: function() {
-    console.log('argument 1', arguments[0]);
     // create an object with queries that can be used to retrieve objects
     const queries =
     {
@@ -135,11 +146,11 @@ module.exports = {
               "PREFIX edm: <http://www.europeana.eu/schemas/edm/> " +
               "PREFIX ore: <http://www.openarchives.org/ore/terms/> " +
               "PREFIX dc: <http://purl.org/dc/elements/1.1/> " +
-              "SELECT ?object ?image " +
+              "SELECT ?aggregation ?object ?view " +
               "WHERE {" +
                 "?object rdf:type edm:ProvidedCHO . " +
                 "?aggregation edm:aggregatedCHO ?object . " +
-                "?aggregation edm:isShownBy ?image . " +
+                "?aggregation edm:isShownBy ?view . " +
                 "?object dc:description ?description . " +
                 `FILTER regex(?description, \"${arguments[0]}\", \"i\") ` +
               "} " +
